8f0db69bcfe317487da1ecd7f3fc46490a0f6d20
felipe <felipe.devfull@gmail.com>
2024-01-21 12:19:44 -0300

    Swagger = Documentação da api.

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -5,6 +5,9 @@ import { GetJobController } from "../controller/GetJobController";
 import { GetOneJobController } from "../controller/GetJobOneController";
 import { AlterJobController } from "../controller/AlterJobController";
 import { DeleteJobController } from "../controller/DeleteJobController";
+//swagger
+import swaggerUi from "swagger-ui-express"
+import swaggerDocument from "../../swagger.json"
 
 import { Validate } from "../middleware/Validate";
 
@@ -15,5 +18,7 @@ Routes.get("/v1/jobs", new GetJobController().getAll);
 Routes.get("/v1/jobs/:id", new GetOneJobController().getOne);
 Routes.put("/v1/jobs/:id",Validate, new AlterJobController().alter);
 Routes.delete("/v1/jobs/:id", new DeleteJobController().delete);
+//swagger
+Routes.use('/v1/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 export default Routes;
